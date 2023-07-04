7045ac9caedcd2ed2a27223698d16349f4fd9a44
parikshit-coditas <parikshit.nikam@coditas.com>
2023-07-04 12:53:14 +0530

    Initial Commit

---
+++ GitSSHManager/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  GitSSHManager
+//
+//  Created by Coditas on 04/07/23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
